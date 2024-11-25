48138ba48fdc39010ae4327b001e32682b94fa86
jfChiluiza <jose_newboy@hotmail.com>
2024-11-25 10:33:51 -0500

    ejercicios

---
+++ mayoramenor.py
@@ -0,0 +1,12 @@
+# Solicitar un número al usuario
+numero = float(input("Ingresa un número: "))
+
+# Determinar si es mayor o menor que 10
+if numero > 10:
+    print("Es mayor que 10.")
+elif numero < 10:
+    print("Es menor que 10.")
+else:
+    print("Es igual a 10.")
+
+    
